86456166df0da453a63ca6261d5c2127a941b08d
Eric Reale <ecr5083@gmail.com>
2019-10-28 19:48:02 +0800

    added the validations & associations to the models

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,8 @@
 class Review < ApplicationRecord
   belongs_to :booking
+
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: [1, 2, 3, 4, 5] }
+  validates_numericality_of :rating
 end
