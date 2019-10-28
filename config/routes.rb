aab82f293a0fdefae961dac553cbd5e69fe6f03a
sacha mestdagh <sacha.mestdagh@hotmail.com>
2019-10-28 17:20:18 +0800

    routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :equipments do
+    resources :bookings
+  end
+
+  resources :bookings, only: [] do
+    resources :reviews, only: [:new, :create]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
