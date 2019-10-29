60a35dd1b7c986a16af8ac3b0b0b8301c0eb26c6
sacha mestdagh <sacha.mestdagh@hotmail.com>
2019-10-29 14:44:41 +0800

    show camera

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,5 +1,4 @@
 class Camera < ApplicationRecord
-
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_many :reviews, through: :bookings
@@ -7,5 +6,4 @@ class Camera < ApplicationRecord
   validates :name, :address, :description, :price_per_day, presence: true
   validates_numericality_of :price_per_day
   mount_uploader :photo, PhotoUploader
-
 end
