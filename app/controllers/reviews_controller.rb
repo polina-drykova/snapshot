ae0e3b24a93027ed9a5ea2f72299e82b58065914
Sebastian Langer <seb.langer@outlook.com>
2019-10-29 05:07:36 +0100

    added review methods

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,22 +1,22 @@
 class ReviewsController < ApplicationController
-  def index
-  end
+  before_action :set_booking, only: [:new, :create]
 
   def new
+    @review = Review.new
   end
 
   def create
+    review = Review.new(review_params)
+    @review.booking = @booking
+    review.save
+    redirect_to camera_booking_path
   end
 
-  def show
+  def set_booking
+    @booking = Booking.find(params[:booking_id])
   end
 
-  def edit
-  end
-
-  def update
-  end
-
-  def destroy
+  def review_params
+    params_require(:review).permit(:content, :rating)
   end
 end
