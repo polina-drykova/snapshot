efce0d3fe7d4d33529f054ad13c125204ef15a20
sacha mestdagh <sacha.mestdagh@hotmail.com>
2019-10-29 22:52:17 +0800

    show now have the book this camera now features (horrible css to be changed) and you get the quote for your booking

--- app/models/camera.rb
+++ app/models/camera.rb
@@ -1,10 +1,16 @@
 class Camera < ApplicationRecord
 
   belongs_to :user
+  has_many :bookings
   has_many :reviews, through: :bookings
 
   validates :name, :address, :description, :price_per_day, presence: true
   validates_numericality_of :price_per_day
   mount_uploader :photo, PhotoUploader
 
+ def unavailable_dates
+    bookings.pluck(:rental_date, :return_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
